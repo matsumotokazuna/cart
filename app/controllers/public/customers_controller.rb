b160f8e4f69d041ecdd68de7ba0276dad09e43e2
vagrant <vagrant@localhost.localdomain>
2020-05-06 08:13:28 +0000

    [add]preparation-4

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,16 @@
 class Public::CustomersController < Public::Base
+    def edit
+        @customer = Customer.find(params[:id])
+    end
+
+    def update
+        @customer = Customer.find(params[:id])
+        @customer.update(customer_params)
+        redirect_to edit_customer_path(@customer)
+    end
+
+    private
+    def customer_params
+        params.require(:customer).permit(:id, :email, :last_name, :first_name, :postcode, :address)
+    end
 end
